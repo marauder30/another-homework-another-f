966dd0d1166d697a8a81061f11fe07efc352463a
Nick Koch <marauder30@gmail.com>
2019-12-16 21:15:57 -0700

    added hiscores and question files, and psuedocode

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      ];
---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+//variable for start button 
+//variable for high score / page
+
+//load opening page (index.html) 
+//set/reset timer to 0
+//display start button
+//link to high scores
+
+
+//click listener on start button begins question array
+//set timer to 75
+//begin counting down from 75 in increments of one second
+//display random question from array? or in a series, whichever is easier
+//question should also randomly display possible answers from array, 1 of which is correct
+
+//click event on each of 4 answer buttons
+// IF a question is answered correctly, add to the players score
+//    refresh page? 
+//    move on to the next question 
+// ELSE IF
+//    a question is answered incorrectly, remove 10 seconds
+//    next question
+
+// when timer reaches zero, clear interval? to end game
+// modal box to collect name and score in local storage
+// store high scores in existing or new variable 
+// href to  high scores page
