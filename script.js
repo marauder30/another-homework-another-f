b7b6e59c2a97cb5d2e81377b01e700aa4a4cd1bd
Nick Koch <marauder30@gmail.com>
2019-12-30 16:20:47 -0700

    added functions for questions 1-5

--- script.js
+++ script.js
@@ -121,6 +121,8 @@ var highScores = document.querySelector(".highscores");
 var modal = document.querySelector("staticBackdrop");
 var modalHeader = document.getElementById("staticBackdropLabel");
 var modalBody = document.getElementById("staticBackdropBody");
+var score;
+var time;
 
 init();
 
@@ -153,7 +155,6 @@ function Decrement() {
 function countdown() { 
     timeRemaining = 75;
     timeRemaining.val = 75;
-    console.log(timeRemaining);
     setTimeout('Decrement()', 75); 
 } 
     
@@ -200,28 +201,215 @@ function renderQuestion1() {
 
         if (element.matches("button") === true) {
 
-            console.log(element);
-            console.log(element.textContent);
-
             if (element.textContent === "alerts") {
 
                 console.log("success!");
+                score += 15;
             } else {
                 console.log("wrong!!");
+                time -= 15;
+            }
+
+            function renderQuestion2() {
+            
+                modalHeader.innerHTML = "";
+                modalBody.innerHTML = "";
+                modalHeader.innerHTML = questions[1].title;
+            
+                var answer1 = document.createElement("button");
+                var answer2 = document.createElement("button");
+                var answer3 = document.createElement("button");
+                var answer4 = document.createElement("button");
+            
+                modalBody.appendChild(answer1);
+                answer1.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                modalBody.appendChild(answer2);
+                answer2.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                modalBody.appendChild(answer3);
+                answer3.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                modalBody.appendChild(answer4);
+                answer4.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+            
+                answer1.textContent = questions[1].choices[0];
+                answer2.textContent = questions[1].choices[1];
+                answer3.textContent = questions[1].choices[2];
+                answer4.textContent = questions[1].choices[3];
+            
+                modalBody.addEventListener("click", function(event) {
+                    var element1 = event.target;
+            
+                    if (element1.matches("button") === true) {
+            
+                        if (element1.textContent === "2") {
+            
+                            console.log("success!");
+                            score += 15;
+                        } else {
+                            console.log("wrong!!");
+                            time -= 15;
+                        }
+                        
+                        function renderQuestion3() {
+                        
+                            modalHeader.innerHTML = "";
+                            modalBody.innerHTML = "";
+                            modalHeader.innerHTML = questions[2].title;
+                        
+                            var answer1 = document.createElement("button");
+                            var answer2 = document.createElement("button");
+                            var answer3 = document.createElement("button");
+                            var answer4 = document.createElement("button");
+                        
+                            modalBody.appendChild(answer1);
+                            answer1.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                            modalBody.appendChild(answer2);
+                            answer2.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                            modalBody.appendChild(answer3);
+                            answer3.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                            modalBody.appendChild(answer4);
+                            answer4.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                        
+                            answer1.textContent = questions[2].choices[0];
+                            answer2.textContent = questions[2].choices[1];
+                            answer3.textContent = questions[2].choices[2];
+                            answer4.textContent = questions[2].choices[3];
+                        
+                            modalBody.addEventListener("click", function(event) {
+                                var element2 = event.target;
+                        
+                                if (element2.matches("button") === true) {
+                        
+                                    if (element2.textContent === "alerts") {
+                        
+                                        console.log("success!");
+                                        score += 15;
+                                    } else {
+                                        console.log("wrong!!");
+                                        time -= 15;
+                                    }
+                        
+                                    function renderQuestion4() {
+                                    
+                                        modalHeader.innerHTML = "";
+                                        modalBody.innerHTML = "";
+                                        modalHeader.innerHTML = questions[3].title;
+                                    
+                                        var answer1 = document.createElement("button");
+                                        var answer2 = document.createElement("button");
+                                        var answer3 = document.createElement("button");
+                                        var answer4 = document.createElement("button");
+                                    
+                                        modalBody.appendChild(answer1);
+                                        answer1.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                        modalBody.appendChild(answer2);
+                                        answer2.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                        modalBody.appendChild(answer3);
+                                        answer3.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                        modalBody.appendChild(answer4);
+                                        answer4.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                    
+                                        answer1.textContent = questions[3].choices[0];
+                                        answer2.textContent = questions[3].choices[1];
+                                        answer3.textContent = questions[3].choices[2];
+                                        answer4.textContent = questions[3].choices[3];
+                                    
+                                        modalBody.addEventListener("click", function(event) {
+                                            var element3 = event.target;
+                                    
+                                            if (element3.matches("button") === true) {
+                                    
+                                                if (element3.textContent === "alerts") {
+                                    
+                                                    console.log("success!");
+                                                    score += 15;
+                                                } else {
+                                                    console.log("wrong!!");
+                                                    time -= 15;
+                                                }
+                                    
+                                                function renderQuestion5() {
+                                                
+                                                    modalHeader.innerHTML = "";
+                                                    modalBody.innerHTML = "";
+                                                    modalHeader.innerHTML = questions[4].title;
+                                                
+                                                    var answer1 = document.createElement("button");
+                                                    var answer2 = document.createElement("button");
+                                                    var answer3 = document.createElement("button");
+                                                    var answer4 = document.createElement("button");
+                                                
+                                                    modalBody.appendChild(answer1);
+                                                    answer1.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                                    modalBody.appendChild(answer2);
+                                                    answer2.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                                    modalBody.appendChild(answer3);
+                                                    answer3.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                                    modalBody.appendChild(answer4);
+                                                    answer4.setAttribute("class", "btn btn-secondary btn-lg btn-block");
+                                                
+                                                    answer1.textContent = questions[4].choices[0];
+                                                    answer2.textContent = questions[4].choices[1];
+                                                    answer3.textContent = questions[4].choices[2];
+                                                    answer4.textContent = questions[4].choices[3];
+                                                
+                                                    modalBody.addEventListener("click", function(event) {
+                                                        var element4 = event.target;
+                                                
+                                                        if (element4.matches("button") === true) {
+                                                
+                                                            if (element4.textContent === "alerts") {
+                                                
+                                                                console.log("success!");
+                                                                score += 15;
+                                                            } else {
+                                                                console.log("wrong!!");
+                                                                time -= 15;
+                                                            }
+                                                
+                                                            // renderHighScores 
+                                                
+                                                        }
+                                                    })
+                                                }
+
+                                                renderQuestion5();
+                                    
+                                            }
+                                        })
+                                    }
+
+                                    renderQuestion4();
+                        
+                                }
+                            })
+                        }
+
+                        renderQuestion3();
+            
+                    }
+                })
             }
 
             renderQuestion2();
 
         }
     })
-
 }
 
 
 
 
 
-// event listener for modal/ check if buttons
+
+
+
+
+
+
+
+
+
+
 
 
 
