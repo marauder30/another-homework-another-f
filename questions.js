796205376ff048c4b2f609a372eb3ee3bd0bbe7d
Nick Koch <marauder30@gmail.com>
2020-01-05 15:53:48 -0700

    updated questions array

--- questions.js
+++ questions.js
@@ -5,24 +5,24 @@ var questions = [
         answer: "alerts"
       },
       {
-          title: "How many ampersands are used to denote an 'and' statement?",
-          choices: ["0", "1", "2", "3"],
-          answer: "2"
+          title: "Which event occurs when the user clicks on an HTML element?",
+          choices: ["onmouseclick", "onchange", "onclick", "onmouseover"],
+          answer: "onclick"
         },
         {
-          title: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts"
+          title: "What is the correct way to write a JavaScript array?",
+          choices: ["var colors = (1:'red', 2:'green' 3:blue')", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "var colors = 'red', 'green', 'blue'", "var colors = ['red', 'green', 'blue']"],
+          answer: "var colors = ['red', 'green', 'blue']"
         },
         {
-          title: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts"
+          title: "How do you round the number 7.25, to the nearest integer?",
+          choices: ["rnd(7.25)", "Math.round(7.25)", "Math.rnd(7.25)", "round(7.25)"],
+          answer: "Math.round(7.25)"
         },
         {
-          title: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts"
+          title: "How do you find the number with the highest value of x and y?",
+          choices: ["ceil(x,y)", "top(x,y)", "Math.ceil(x,y)", "Math.max(x,y)"],
+          answer: "Math.max(x,y)"
         },
         ];
 
